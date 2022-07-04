0cd2ebe34fabaddfd9ea3dcf0bd695c075222838
Aleksei Novikov <alex.nov@list.ru>
2022-07-04 18:46:58 +0300

    Task

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,10 +5,13 @@
  лучше обойтись исключительно массивами
 */ 
 
-void methodArray (string [] array){
+void methodArray (string [] array)
+{
      string[] newarr = new string[array.Length];
-     for (int i=0; i<array.Length; i++){      
-        if(array[i].Length<=3){
+     for (int i=0; i<array.Length; i++)
+     {      
+        if(array[i].Length<=3)
+        {
         newarr[i]=array[i];;
         Console.WriteLine(newarr[i]); 
         }
@@ -16,8 +19,10 @@ void methodArray (string [] array){
         Console.WriteLine(); 
 }
 
-void Print(string [] Array){
-    for (int i=0; i<Array.Length; i++){
+void Print(string [] Array)
+{
+    for (int i=0; i<Array.Length; i++)
+    {
         Console.WriteLine(Array[i]);
     }
     Console.WriteLine();
